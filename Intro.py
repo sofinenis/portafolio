6d5544cf35c01219e00d52cbf784cb14e7c61492
sofinenis <sofiacg004@gmail.com>
2025-10-31 05:02:08 -0500

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -71,11 +71,13 @@ st.markdown("""
 </div>
 """, unsafe_allow_html=True)
 
-st.write("")  # Espacio visual
+st.write("")
 
 # ==============================
 # DATOS DE LAS 15 TARJETAS
 # ==============================
+base_url = "https://raw.githubusercontent.com/tuusuario/mi-proyecto/main/images/"
+
 titles = [
     "Intro", "Traductor", "texto a voz", "Reconocimiento de imagen", "Analisis de sentimiento",
     "Analisis de texto ESP", "Analisis texto ING", "Reconocimiento de objetos", "Reconocimiento de gestos", "Chat PDF",
@@ -83,9 +85,9 @@ titles = [
 ]
 
 images = [
-    "girasol.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg",
-    "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg",
-    "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg"
+    f"{base_url}girasol.jpg", f"{base_url}1.jpg", f"{base_url}2.jpg", f"{base_url}3.jpg", f"{base_url}4.jpg",
+    f"{base_url}5.jpg", f"{base_url}6.jpg", f"{base_url}7.jpg", f"{base_url}8.jpg", f"{base_url}9.jpg",
+    f"{base_url}10.jpg", f"{base_url}11.jpg", f"{base_url}12.jpg", f"{base_url}13.jpg", f"{base_url}14.jpg"
 ]
 
 links = [
